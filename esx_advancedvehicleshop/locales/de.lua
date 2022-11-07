4196aef792410af3d58f9331799dcfda3c166d35
HADES/ <108996607+HADES-96@users.noreply.github.com>
2022-11-07 12:16:41 +0100

    Add german translation
    Add german translation

---
+++ esx_advancedvehicleshop/locales/de.lua
@@ -0,0 +1,69 @@
+Locales['de'] = {
+	-- Gemeinsame
+	['vehicle_dealer'] = 'Autohaus',
+	['shop_awaiting_model'] = 'das Fahrzeug wird gerade ausgeparkt, ~r~WARTE BITTE!',
+	['no'] = 'Nein',
+	['yes'] = 'Ja',
+	['finance'] = 'Finanzierung',
+	['payfull'] = 'In voller Höhe bezahlen',
+	['cancel'] = 'Kauf stornieren',
+	['not_enough_money'] = 'Du hast nicht genug Geld',
+	['not_yours'] = 'Dieses Fahrzeug gehört nicht Dir',
+	['sell_menu'] = 'Drücke ~INPUT_CONTEXT~ um Ihr ~y~%s~s~ für ~g~%s EUR~s~ zu verkaufen',
+	['generic_shopitem'] = '%s EUR',
+	['buy_vehicle'] = 'Möchtest du %s für %s EUR kaufen?',
+	['sold_for'] = 'das ~b~%s~s~ wurde ~y~s~ verkauft~s~ für ~g~$%s~s~',
+	['go_to'] = 'Sie müssen zu einem Lizenzshop gehen, um Lizenzen zu kaufen.',
+	['your_rank'] = 'Dieses Fahrzeug ist nicht für Ihren Rang geeignet!',
+	['your_job'] = 'Dieses Fahrzeug ist nicht für Ihren Beruf geeignet!',
+	['cant_sell'] = 'Du musst dieses Fahrzeug dort verkaufen, wo du es gekauft hast.',
+	['spawnpoint_blocked'] = 'Fahrzeug-Spawnpunkt ist ~r~blocked~s~!',
+	['vehicle_belongs'] = 'Das %s mit dem Kennzeichen ~y~%s~s~ gehört jetzt ~b~DIR~s~',
+
+	-- Ambulanz-Shop
+	['ambulance_menu'] = 'Drücke ~INPUT_CONTEXT~ um den ~r~Krankenhaus Shop zu erreichen',
+	['ambulance_dealer'] = 'Autohaus | Krankenwagen',
+
+	-- Polizei-Shop
+	['police_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~b~Polizei Shop zu betreten',
+	['police_dealer'] = 'Autohaus | Polizei',
+
+	-- Abteilung Shop
+	['division_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~b~Division Shop zu betreten',
+	['division_dealer'] = 'Autohaus | Abteilung',
+
+	-- Mechanic Shop
+	['mechanic_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~o~Mechaniker Shop zu betreten',
+	['mechanic_dealer'] = 'Autohaus | Mechaniker',
+
+	-- Taxi-Shop
+	['taxi_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~o~Taxi Shop zu betreten',
+	['taxi_dealer'] = 'Autohaus | Taxi',
+
+	-- Flugzeug-Shop
+	['aircraft_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~p~Flugzeuge-Shop zu betreten',
+	['aircraft_dealer'] = 'Händler | Flugzeuge',
+	['aircraft_missing'] = 'Du hast keinen Luftfahrzeugschein!',
+
+	-- Bootsgeschäft
+	['boat_menu'] = 'Drücke ~INPUT_CONTEXT~, um den ~b~Boot Shop zu öffnen',
+	['boat_dealer'] = 'Händler | Boot',
+	['boat_missing'] = 'Du hast keinen Bootsführerschein!',
+
+	-- Autohaus
+	['car_menu'] = 'Drücke ~INPUT_CONTEXT~, um den ~g~Autoladen aufzurufen',
+	['car_dealer'] = 'Autohaus | Auto',
+	['car_missing'] = 'Du hast keinen Führerschein!',
+
+	-- Truck Shop
+	['truck_menu'] = 'Drücke ~INPUT_CONTEXT~, um den ~c~Truck Shop zu erreichen',
+	['truck_dealer'] = 'Autohaus | Truck',
+	['truck_missing'] = 'Du hast keinen Gewerbeschein!',
+
+	-- VIP-Shop
+	['vipa_menu'] = 'Drücke ~INPUT_CONTEXT~, um den ~y~VIP Flugzeug Shop zu betreten',
+	['vipb_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~y~VIP Boot Shop zu betreten',
+	['vipc_menu'] = 'Drücke ~INPUT_CONTEXT~ um ~y~VIP Auto Shop zu betreten',
+	['vip_dealer'] = 'Autohaus | VIP',
+	['vip_no'] = 'Du bist ~r~KEIN ~w~ ~y~VIP!',
+}
